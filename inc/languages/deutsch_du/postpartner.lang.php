0ac6c5300d6441d8e7375edd15d2fbf446b4a52b
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-04-25 11:24:16 +0200

    Update postpartner.lang.php

--- inc/languages/deutsch_du/postpartner.lang.php
+++ inc/languages/deutsch_du/postpartner.lang.php
@@ -1,26 +1,26 @@
 <?php
 
 $l['postpartner_navigation_lists'] = "Listen";
-$l['postpartner_navigation'] = "Postpartner Suche";
-$l['postpartner_navigation_edit'] = "Postpartner Suche von {1} bearbeiten";
+$l['postpartner_navigation'] = "Postpartnersuche";
+$l['postpartner_navigation_edit'] = "Partnersuche von {1} bearbeiten";
 
-$l['postpartner_overview_desc'] = "Willkommen bei der Postpartner Suche.<br>
-Hier kannst du, wie der Name schon verrät nach neuen Postpartnern suchen. Entweder erstellst du eine eigene Suche(n) für deinen oder deine Charaktere oder du schaust die die bisherigen Angebote einmal genauer an, ob etwas dabei ist für dich. Die Szenenangebote kannst du sogar, wenn vom Team eingestellt nach Postingvorlieben filtern.<br>
-Eine eigene Suche kannst du nur aufgeben, wenn deine Benutzergruppe dafür berechtigt ist. Wenn dem so ist, dann siehst du unter diesem Erklärtext ein Formular. Hier kannst du nicht nur eintragen, mit welchem Charakter du weitere Szenen suchst, sondern auch was du gerne mit deinem Charakter spielen möchtest und wann die Szene stattfinden soll. Außerdem kannst du eine Szenenkapazität festlegen. Das bedeutet, wie viele Szenen du noch in unterbekommen kannst.<br>
-Du kannst so viele Szenensuchen erstellen wie du möchtest, somit wäre es gar nicht so unpraktisch, wenn du für jede Szenenidee eine eigne Suche veröffentlichst.<br>
-Deine Szenenangebote kannst du jeder Zeit und von allen Accounts bearbeiten und löschen. Genau wie deine Szenenkapazität updaten, wenn eine neue Szene entstanden ist.<br><br>
+$l['postpartner_overview_desc'] = "Willkommen bei der Postpartnersuche.<br>
+Hier kannst du, wie der Name schon verrät, nach neuen Postpartnern suchen. Entweder erstellst du eigene Suche(n) für deinen oder deine Charaktere oder du schaust die bisherigen Angebote einmal genauer an, ob etwas für dich dabei ist. Die Szenenangebote kannst du sogar, wenn vom Team eingestellt, nach Postingvorlieben filtern.<br>
+Eine eigene Suche kannst du nur aufgeben, wenn deine Benutzergruppe dafür berechtigt ist. Wenn dem so ist, dann siehst du unter diesem Erklärtext ein Formular. Hier kannst du nicht nur eintragen, mit welchem Charakter du weitere Szenen suchst, sondern auch, was du gerne mit deinem Charakter spielen möchtest und wann die Szene stattfinden soll. Außerdem kannst du eine Szenenkapazität festlegen. Das bedeutet, wie viele Szenen du noch unterbekommen kannst.<br>
+Du kannst so viele Szenensuchen erstellen wie du möchtest, somit wäre es gar nicht so unpraktisch, wenn du für jede Szenenidee eine eigene Suche veröffentlichst.<br>
+Deine Szenenangebote kannst du jederzeit und von allen Accounts bearbeiten und löschen. Genau wie deine Szenenkapazität updaten, wenn eine neue Szene entstanden ist.<br><br>
 
-Wenn du bei den Postpartner Suchen von anderen Charakteren etwas entdeckt hast, dann hast du zwei Möglichkeiten dein Interesse zu bekunden. Entweder trittst du per PN in Kontakt mit der/dem User*in oder du drückst auf \"Interesse bekunden\" und der/die User*in bekommt auf dem Index eine Benachrichtigung, dass du dich für das Angebot interessierst.<br>
-Solltest du Fragen oder Probleme haben, dann melde dich gerne beim Team. Sonst hab viel Spaß und Freude bei der Postpartner Suche.";
+Wenn du bei den Postpartnersuchen von anderen Charakteren etwas entdeckt hast, dann hast du zwei Möglichkeiten dein Interesse zu bekunden: Entweder trittst du per PN in Kontakt mit der/dem User*in oder du drückst auf \"Interesse bekunden\" und der/die User*in bekommt auf dem Index eine Benachrichtigung, dass du dich für das Angebot interessierst.<br>
+Solltest du Fragen oder Probleme haben, dann melde dich gerne beim Team. Sonst hab viel Spaß und Freude bei der Postpartnersuche.";
 $l['postpartner_overview_searchs_count_singular'] = "{1} Szenenangebot insgesamt";
 $l['postpartner_overview_searchs_count_plural'] = "{1} Szenenangebote insgesamt";
 
 $l['postpartner_dice'] = "<i class=\"fa-solid fa-dice\"></i> Postpartnerwürfel";
 $l['postpartner_dice_headline'] = "ausgewürftelter Postpartner:";
 
-$l['postpartner_add_headline'] = "Neue Postpartner Suche aufgeben";
+$l['postpartner_add_headline'] = "Neue Postpartnersuche aufgeben";
 $l['postpartner_add_character'] = "Charakter";
-$l['postpartner_add_character_desc'] = "Für welchen Charakter willst du eine neue Postpartner Suche posten?";
+$l['postpartner_add_character_desc'] = "Für welchen Charakter willst du eine neue Postpartnersuche posten?";
 $l['postpartner_add_character_select'] = "Charakter auswählen";
 $l['postpartner_add_maxcount'] = "Szenenkapazität";
 $l['postpartner_add_maxcount_desc'] = "Für wie viele Szenen hast du noch Platz? Gib hier die maximal Summe an.";
@@ -28,10 +28,10 @@ $l['postpartner_add_maxcount_check'] = "keine Szenenbegrenzung";
 $l['postpartner_add_inplay'] = "Inplayzeitraum";
 $l['postpartner_add_inplay_desc'] = "In welchem Zeitraum möchtest du die Szene(n) spielen?";
 $l['postpartner_add_inplay_value'] = "März";
-$l['postpartner_add_searchdesc'] = "Szenen Vorschläge";
+$l['postpartner_add_searchdesc'] = "Szenenvorschläge";
 $l['postpartner_add_searchdesc_desc'] = "Welche Art von Szenen möchtest du posten?";
 $l['postpartner_add_button'] = "Suche veröffentlichen";
-$l['postpartner_edit_headline'] = "Postpartner Suche bearbeiten";
+$l['postpartner_edit_headline'] = "Postpartnersuche bearbeiten";
 $l['postpartner_edit_button'] = "Suche bearbeiten";
 
 $l['postpartner_own_headline'] = "deine Szenenangebote";
@@ -40,9 +40,9 @@ $l['postpartner_own_delete'] = "Löschen";
 $l['postpartner_own_edit'] = "Bearbeiten";
 $l['postpartner_own_plusScene'] = "Szene entstanden";
 
-$l['postpartner_own_none'] = "Aktuelle hast du keine Szenensuche offen.";
+$l['postpartner_own_none'] = "Aktuell hast du keine Szenensuche offen.";
 $l['postpartner_searchs_none'] = "Es kann keine passende Szenensuche angezeigt werden.
-<br>Entweder gibt es aktuell keine aktiv eingetragenen Angebote oder die Filterangaben passen nicht mit Angaben der User überein. 
+<br>Entweder gibt es aktuell keine aktiv eingetragenen Angebote oder die Filterangaben passen nicht mit den Angaben der User überein.
 <br>Oder Anfragen für oder von dir für Szenen liegen noch unbeantwortet vor.";
 
 $l['postpartner_scenecount_open'] = "keine Szenenbegrenzung";
@@ -59,19 +59,19 @@ $l['postpartner_filter_postingperspective'] = "Alle Postperspektiven";
 $l['postpartner_options_interested'] = "Interesse bekunden mit {1}";
 $l['postpartner_options_pn'] = "PN schreiben";
 
-$l['postpartner_redirect_deleteSearch'] = "Du hast erfolgreich die Szenensuche von <b>{1}</b> gelöscht! Du  wirst nun zurück auf die Seite von der Postpartner Suche geleitet.";
-$l['postpartner_redirect_deleteSearch_error'] = "Du wolltest eine Postpartner Suche löschen, welche nicht von dir ist! Die Suche wurde nicht gelöscht und du wirst nun zurück auf die Seite von der Postpartner Suche geleitet.";
-$l['postpartner_redirect_plusScene'] = "Du hast erfolgreich die Szenenkapazität für <b>{1}</b> geupdatet! Du  wirst nun zurück auf die Seite von der Postpartner Suche geleitet.";
-$l['postpartner_redirect_plusScene_error'] = "Du wolltest die Szenenkapazitätr von einem Charakter updaten, der nicht dir gehört! Die Szenenkapazität wurde nicht verändert und du wirst nun zurück auf die Seite von der Postpartner Suche geleitet.";
-$l['postpartner_redirect_interestedUser'] = "Du hast erfolgreich dein Interesse an einer Szene gezeigt! Der User wurde dadrüber benachrichtet. Du  wirst nun zurück auf die Seite von der Postpartner Suche geleitet.";
+$l['postpartner_redirect_deleteSearch'] = "Du hast erfolgreich die Szenensuche von <b>{1}</b> gelöscht! Du  wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_deleteSearch_error'] = "Du wolltest eine Postpartnersuche löschen, welche nicht von dir ist! Die Suche wurde nicht gelöscht und du wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_plusScene'] = "Du hast erfolgreich die Szenenkapazität für <b>{1}</b> geupdatet! Du  wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_plusScene_error'] = "Du wolltest die Szenenkapazität von einem Charakter updaten, der nicht dir gehört! Die Szenenkapazität wurde nicht verändert und du wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_interestedUser'] = "Du hast erfolgreich dein Interesse an einer Szene gezeigt! Der User wurde dadrüber benachrichtigt. Du  wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
 $l['postpartner_redirect_acceptRequest'] = "Du hast dich entschieden eine Szene mit <b>{1}</b> zu eröffnen! Du wirst nun in die Inplaykategorie geleitet, damit du die Szene direkt eröffnen kannst. Deine Szenenkapazität wurde entsprechend geupdatet.";
-$l['postpartner_redirect_acceptRequest_error'] = "Du wolltest eine Szenenanfrage bestätigen, welche nicht zu dir gehört! Die Anfrage wurde nicht bestätigt und auch die Szenenkapazität wurde nicht verändert und du wirst nun zurück auf den Index geleitet.";
-$l['postpartner_redirect_add'] = "Du hast erfolgreich eine neu Postpartner Suche für <b>{1}</b> gepostet! Du wirst nun zurück auf die Seite von der Postpartner Suche geleitet.";
-$l['postpartner_redirect_edit'] = "Du hast erfolgreich die Postpartner Suche für <b>{1}</b> bearbeitet! Du wirst nun zurück auf die Seite von der Postpartner Suche geleitet.";
-$l['postpartner_redirect_rejectRequest'] = "Du hast erfolgreich eine PN an <b>{1}</b> mit einem Ablehnung für das Szenenangebot verschickt! Du wirst nun zurück auf deine ursprüngliche Seite geleitet.";
+$l['postpartner_redirect_acceptRequest_error'] = "Du wolltest eine Szenenanfrage bestätigen, welche nicht zu dir gehört! Die Anfrage wurde nicht bestätigt und auch die Szenenkapazität wurde nicht verändert. Du wirst nun zurück auf den Index geleitet.";
+$l['postpartner_redirect_add'] = "Du hast erfolgreich eine neu Postpartnersuche für <b>{1}</b> gepostet! Du wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_edit'] = "Du hast erfolgreich die Postpartnersuche für <b>{1}</b> bearbeitet! Du wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_rejectRequest'] = "Du hast erfolgreich eine PN an <b>{1}</b> mit einer Ablehnung für das Szenenangebot verschickt! Du wirst nun zurück auf deine ursprüngliche Seite geleitet.";
 
-$l['postpartner_pm_subject'] = "Postpartner Suche - Ablehnung";
-$l['postpartner_pm_message'] = "Leider hat {1} kein Interesse an einer Szenen mit dir ausfolgendem Grund:<br>{2}";
+$l['postpartner_pm_subject'] = "Postpartnersuche - Ablehnung";
+$l['postpartner_pm_message'] = "Leider hat {1} aus folgendem Grund kein Interesse an einer Szenen mit dir:<br>{2}";
 
 $l['postpartner_header_text'] = "<strong>{1}</strong> hat Interesse an einer Szene mit {2}.<br>
 Ihr solltet per PN oder Discord Kontakt aufnehmen und die Szene planen.";
@@ -79,12 +79,12 @@ $l['postpartner_header_accept'] = "<i class=\"fa-regular fa-check\"></i> Szene e
 $l['postpartner_header_rejectlink'] = "<i class=\"fa-regular fa-xmark\"></i> kein Interesse";
 $l['postpartner_header_reject_headline'] = "Ablehnungsgrund";
 $l['postpartner_header_reject_pm'] = "PN-Inhalt";
-$l['postpartner_header_reject_pm_desc'] = "Gebe hier einen ausführlichen Grund an, wieso du kein Interesse daran hast mit diesem User/Charakter eine Szene zu starten. Der Charakter bekommt deinen Grund per PN zugeschickt.";
+$l['postpartner_header_reject_pm_desc'] = "Gebe hier einen ausführlichen Grund an, wieso du kein Interesse daran hast, mit diesem Charakter eine Szene zu starten. Der Charakter bekommt deinen Grund per PN zugeschickt.";
 $l['postpartner_header_reject_button'] = "Szenenanfrage ablehnen";
 
-$l['postpartner_online_location'] = "Schaut sich gerade die Postpartner Suche an.";
-$l['postpartner_online_location_edit'] = "Bearbeitet gerade die Postpartner Suche von {1}";
+$l['postpartner_online_location'] = "Schaut sich gerade die Postpartnersuche an.";
+$l['postpartner_online_location_edit'] = "Bearbeitet gerade die Postpartnersuche von {1}";
 
-$l['postpartner_new'] = "{1} hat sich in die Postpartner Suche eingetragen.";
-$l['myalerts_setting_postpartner_new'] = "Benachrichtigung erhalten, wenn jemand neues sich in die Postpartner Suche eingetragen hat?";
+$l['postpartner_new'] = "{1} hat sich in die Postpartnersuche eingetragen.";
+$l['myalerts_setting_postpartner_new'] = "Benachrichtigung erhalten, wenn jemand neues sich in die Postpartnersuche eingetragen hat?";
 ?>
