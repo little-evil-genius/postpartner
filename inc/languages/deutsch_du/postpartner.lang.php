bbc57552d1397c18b497f8b423a6697422c16720
Lara <75126217+little-evil-genius@users.noreply.github.com>
2023-04-25 08:41:53 +0200

    Create postpartner.lang.php

---
+++ inc/languages/deutsch_du/postpartner.lang.php
@@ -0,0 +1,83 @@
+<?php
+
+$l['postpartner_navigation_lists'] = "Listen";
+$l['postpartner_navigation'] = "Postpartnersuche";
+$l['postpartner_navigation_edit'] = "Postpartnersuche von {1} bearbeiten";
+
+$l['postpartner_overview_desc'] = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+$l['postpartner_overview_searchs_count_singular'] = "{1} Szenenangebot insgesamt";
+$l['postpartner_overview_searchs_count_plural'] = "{1} Szenenangebote insgesamt";
+
+$l['postpartner_dice'] = "<i class=\"fa-solid fa-dice\"></i> Postpartnerwürfel";
+$l['postpartner_dice_headline'] = "ausgewürftelter Postpartner:";
+
+$l['postpartner_add_headline'] = "Neue Postpartnersuche aufgeben";
+$l['postpartner_add_character'] = "Charakter";
+$l['postpartner_add_character_desc'] = "Für welchen Charakter willst du eine neue Postpartnersuche posten?";
+$l['postpartner_add_character_select'] = "Charakter auswählen";
+$l['postpartner_add_maxcount'] = "Szenenkapazität";
+$l['postpartner_add_maxcount_desc'] = "Für wie viele Szenen hast du noch Platz? Gib hier die maximal Summe an.";
+$l['postpartner_add_maxcount_check'] = "keine Szenenbegrenzung";
+$l['postpartner_add_inplay'] = "Inplayzeitraum";
+$l['postpartner_add_inplay_desc'] = "In welchem Zeitraum möchtest du die Szene(n) spielen?";
+$l['postpartner_add_inplay_value'] = "März";
+$l['postpartner_add_searchdesc'] = "Szenen Vorschläge";
+$l['postpartner_add_searchdesc_desc'] = "Welche Art von Szenen möchtest du posten?";
+$l['postpartner_add_button'] = "Suche veröffentlichen";
+$l['postpartner_edit_headline'] = "Postpartnersuche bearbeiten";
+$l['postpartner_edit_button'] = "Suche bearbeiten";
+
+$l['postpartner_own_headline'] = "deine Szenenangebote";
+$l['postpartner_own_inplay'] = "Inplayzeitraum:";
+$l['postpartner_own_delete'] = "Löschen";
+$l['postpartner_own_edit'] = "Bearbeiten";
+$l['postpartner_own_plusScene'] = "Szene entstanden";
+
+$l['postpartner_own_none'] = "Aktuelle hast du keine Szenensuche offen.";
+$l['postpartner_searchs_none'] = "Es kann keine passende Szenensuche angezeigt werden.
+<br>Entweder gibt es aktuell keine aktiv eingetragenen Angebote oder die Filterangaben passen nicht mit Angaben der User überein. 
+<br>Oder Anfragen für oder von dir für Szenen liegen noch unbeantwortet vor.";
+
+$l['postpartner_scenecount_open'] = "keine Szenenbegrenzung";
+$l['postpartner_scenecount'] = "{1}/{2} Szenen noch frei";
+$l['postpartner_scenecount_edit_singular'] = "<b>Erinnerung:</b> {1} Szene ist schon entstanden";
+$l['postpartner_scenecount_edit_plural'] = "<b>Erinnerung:</b> {1} Szenen sind schon entstanden";
+
+$l['postpartner_filter'] = "Filtern nach:";
+$l['postpartner_filter_button'] = "Szenenangebote filtern";
+$l['postpartner_filter_postinglength'] = "Alle Postinglängen";
+$l['postpartner_filter_postingfrequency'] = "Alle Postfrequenzen";
+$l['postpartner_filter_postingperspective'] = "Alle Postperspektiven";
+
+$l['postpartner_options_interested'] = "Interesse bekunden mit {1}";
+$l['postpartner_options_pn'] = "PN schreiben";
+
+$l['postpartner_redirect_deleteSearch'] = "Du hast erfolgreich die Szenensuche von <b>{1}</b> gelöscht! Du  wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_deleteSearch_error'] = "Du wolltest eine Postpartnersuche löschen, welche nicht von dir ist! Die Suche wurde nicht gelöscht und du wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_plusScene'] = "Du hast erfolgreich die Szenenkapazität für <b>{1}</b> geupdatet! Du  wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_plusScene_error'] = "Du wolltest die Szenenkapazitätr von einem Charakter updaten, der nicht dir gehört! Die Szenenkapazität wurde nicht verändert und du wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_interestedUser'] = "Du hast erfolgreich dein Interesse an einer Szene gezeigt! Der User wurde dadrüber benachrichtet. Du  wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_acceptRequest'] = "Du hast dich entschieden eine Szene mit <b>{1}</b> zu eröffnen! Du wirst nun in die Inplaykategorie geleitet, damit du die Szene direkt eröffnen kannst. Deine Szenenkapazität wurde entsprechend geupdatet.";
+$l['postpartner_redirect_acceptRequest_error'] = "Du wolltest eine Szenenanfrage bestätigen, welche nicht zu dir gehört! Die Anfrage wurde nicht bestätigt und auch die Szenenkapazität wurde nicht verändert und du wirst nun zurück auf den Index geleitet.";
+$l['postpartner_redirect_add'] = "Du hast erfolgreich eine neu Postpartnersuche für <b>{1}</b> gepostet! Du wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_edit'] = "Du hast erfolgreich die Postpartnersuche für <b>{1}</b> bearbeitet! Du wirst nun zurück auf die Seite von der Postpartnersuche geleitet.";
+$l['postpartner_redirect_rejectRequest'] = "Du hast erfolgreich eine PN an <b>{1}</b> mit einem Ablehnung für das Szenenangebot verschickt! Du wirst nun zurück auf deine ursprüngliche Seite geleitet.";
+
+$l['postpartner_pm_subject'] = "Postpartnersuche - Ablehnung";
+$l['postpartner_pm_message'] = "Leider hat {1} kein Interesse an einer Szenen mit dir ausfolgendem Grund:<br>{2}";
+
+$l['postpartner_header_text'] = "<strong>{1}</strong> hat Interesse an einer Szene mit {2}.<br>
+Ihr solltet per PN oder Discord Kontakt aufnehmen und die Szene planen.";
+$l['postpartner_header_accept'] = "<i class=\"fa-regular fa-check\"></i> Szene erstellen ";
+$l['postpartner_header_rejectlink'] = "<i class=\"fa-regular fa-xmark\"></i> kein Interesse";
+$l['postpartner_header_reject_headline'] = "Ablehnungsgrund";
+$l['postpartner_header_reject_pm'] = "PN-Inhalt";
+$l['postpartner_header_reject_pm_desc'] = "Gebe hier einen ausführlichen Grund an, wieso du kein Interesse daran hast mit diesem User/Charakter eine Szene zu starten. Der Charakter bekommt deinen Grund per PN zugeschickt.";
+$l['postpartner_header_reject_button'] = "Szenenanfrage ablehnen";
+
+$l['postpartner_online_location'] = "Schaut sich gerade die Postpartnersuche an.";
+$l['postpartner_online_location_edit'] = "Bearbeitet gerade die Postpartnersuche von {1}";
+
+$l['postpartner_new'] = "{1} hat sich in die Postpartnersuche eingetragen.";
+$l['myalerts_setting_postpartner_new'] = "Benachrichtigung erhalten, wenn jemand neues sich in die Postpartnersuche eingetragen hat?";
+?>
